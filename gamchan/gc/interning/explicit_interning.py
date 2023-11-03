205765100bbc149c593205f3b34c1e03f68f5c70
Gamchan Kang <104420634+gsgh3016@users.noreply.github.com>
2023-11-03 17:24:27 +0900

    comment: change wrong comment

--- gamchan/gc/interning/explicit_interning.py
+++ gamchan/gc/interning/explicit_interning.py
@@ -1,10 +1,10 @@
 import sys
 
 a = sys.intern("hello world")  # 명시적으로 intern
-b = "hello world"
+b = "hello world" # 자동 intern
 c = " ".join(["hello", "world"])  # 동적으로 생성
 
-print(a is b)  # False
+print(a is b)  # True
 print(a is c)  # False
 
 b = sys.intern(b)
